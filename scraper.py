32b6b47faad0a8c0bfe5a4f204f1bee0896290b4
m-Kubacki <116362784+m-Kubacki@users.noreply.github.com>
2023-05-12 12:51:07 +0200

    2023-05-12 MK

--- scraper.py
+++ scraper.py
@@ -1,3 +1,5 @@
+import os
+import json
 import requests
 from bs4 import BeautifulSoup
 
@@ -32,14 +34,28 @@ selectors = {
 # product_code = "58835954"
 product_code = "39562616"
 url = f"https://www.ceneo.pl/{product_code}#tab=reviews"
-respons = requests.get(url)
-if  respons.status_code == requests.codes.ok:
-    page_dom = BeautifulSoup(respons.text, 'html.parser')
-    opinions = page_dom.select("div.js_product-review")
-    opinions_all = []
-    for opinion in opinions:
-        single_opinion = {}
-        for key, value in selectors.items():
-            single_opinion[key] = get_cos(opinion, *value)
-        opinions_all.append(single_opinion)
-    print(opinions_all)
+opinions_all = []
+while(url):
+    print(url)
+    respons = requests.get(url)
+    if  respons.status_code == requests.codes.ok:
+        page_dom = BeautifulSoup(respons.text, 'html.parser')
+        opinions = page_dom.select("div.js_product-review")
+        for opinion in opinions:
+            single_opinion = {}
+            for key, value in selectors.items():
+                single_opinion[key] = get_cos(opinion, *value)
+            opinions_all.append(single_opinion)
+    try:
+        url = "https://www.ceneo.pl"+get_cos(page_dom, "a.pagination__next", "href")
+    except TypeError:
+        url = None
+
+try:
+    os.mkdir("opinions")
+except FileExistsError:
+    pass
+
+jf = open(f"opinions/{product_code}.json","w", encoding="UTF-8")
+json.dump(opinions_all, jf, indent=4, ensure_ascii=False )
+jf.close()
